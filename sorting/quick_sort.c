675bea1bcf5dd4969e5e9faa3459e530d63a74bc
Rahul Bhaskar <bhaskar.rahul25@gmail.com>
2019-02-18 17:54:50 +0530

    quick_sort anaysis

--- sorting/quick_sort.c
+++ sorting/quick_sort.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<math.h>
 
+long int count=0;
 
 void swap(int* a, int *b){
     int temp = *a;
@@ -14,13 +16,11 @@ int _partition(int arr[], int low, int high){
     while(1){
         while(i<=high && arr[i]<=pivot){
             i++;
+            count++;
         }
-        while(arr[j]>=pivot){
-            if (i > j)
-            {
-                break;
-            }
+        while(arr[j]>pivot){
             j--;
+            count++;
         }
         if (i >= j)
         {
@@ -44,53 +44,55 @@ void quick_sort(int arr[],int n){
     _quick_sort(arr,0,n-1);
 }
 
-int main()
-{
-    srand(time(0));
-    int arr[100000], i, n;
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
-    {
-        arr[i] = rand() % (n * 10);
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    quick_sort(arr, n);
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    return 0;
-}
-
 // int main()
 // {
 //     srand(time(0));
-//     double test = 10;
-//     int n, i;
-//     int asc[100000], dsc[100000], random[100000];
-//     while (test <= 100000)
+//     int arr[100000], i, n;
+//     scanf("%d", &n);
+//     for (i = 0; i < n; i++)
 //     {
-//         printf("TESTING FOR N=%lf\n", test);
-//         n = test;
-//         for (i = 0; i < n; i++)
-//         {
-//             asc[i] = i + 1;
-//             dsc[i] = n - i;
-//             random[i] = rand() % (10 * n);
-//         }
-//         count=0;
-//         quick_sort(asc, n);
-//         printf("%ld \n", count);
-//         count=0;
-//         quick_sort(dsc, n);
-//         printf("%ld \n", count);
-//         count=0;
-//         quick_sort(random, n);
-//         printf("%ld \n", count);
-//         test *= 10;
-//         printf("\n\n");
+//         arr[i] = rand() % (n * 10);
+//         printf("%d ", arr[i]);
 //     }
+//     printf("\n");
+//     quick_sort(arr, n);
+//     for (i = 0; i < n; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
 //     return 0;
 // }
+
+int main()
+{
+    srand(time(0));
+    double test = 10;
+    int n, i;
+    int asc[100000], dsc[100000], random[100000];
+    while (test <= 100000)
+    {
+        printf("TESTING FOR N=%lf\n", test);
+        n = test;
+        for (i = 0; i < n; i++)
+        {
+            asc[i] = i + 1;
+            dsc[i] = n - i;
+            random[i] = rand() % (10 * n);
+        }
+        count=0;
+        quick_sort(asc, n);
+        printf("%ld \n", count);
+        count=0;
+        quick_sort(dsc, n);
+        printf("%ld \n", count);
+        printf("ASC&DSC: %lf\n",(double)count/(n*n));
+        count=0;
+        quick_sort(random, n);
+        printf("%ld \n", count);
+        printf("RANDOM: %lf\n",(double)count/(n*log10(n)));
+        test *= 10;
+        printf("\n\n");
+    }
+    return 0;
+}
